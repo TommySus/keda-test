6769bc959b9323076b17e776af4c86865602e9e4
tommysusanto77 <tommysusanto77@gmail.com>
2023-02-11 17:19:31 +0700

    done add checkNumber, palindrome, sortNumber and test

---
+++ palindromeCheck.js
@@ -0,0 +1,9 @@
+function palindromeCheck(str) {
+    let reverse = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+      reverse += str[i];
+    }
+    return reverse === str;
+}
+  
+module.exports = palindromeCheck;
